310af6c6957682e73d6e4de799bb23d264c464cf
Oliver Castillo <olcastillot@gmail.com>
2024-07-26 16:06:39 -0500

    feat: add custom exceptions and exception handling

---
+++ src/main/java/com/app/auth/service/impl/auth/CustomAuthEntryPoint.java
@@ -0,0 +1,29 @@
+package com.app.auth.service.impl.auth;
+
+import com.app.auth.model.dto.response.OperationResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthEntryPoint implements AuthenticationEntryPoint {
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  private final Logger logger = LoggerFactory.getLogger(CustomAuthEntryPoint.class);
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    logger.error(authException.getMessage());
+    response.setContentType("application/json");
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    OperationResponse operationResponse = new OperationResponse("Authentication failed");
+    response.getWriter().write(objectMapper.writeValueAsString(operationResponse));
+  }
+}
