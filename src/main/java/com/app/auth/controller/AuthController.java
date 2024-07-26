fac28c8145cb4fd5557a74c9b4fd0b4ed0428ed5
Oliver Castillo <olcastillot@gmail.com>
2024-07-25 21:54:12 -0500

    feat: add controllers

--- src/main/java/com/app/auth/controller/AuthController.java
+++ src/main/java/com/app/auth/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.app.auth.controller;
 
+import com.app.auth.model.dto.request.AuthRequest;
 import com.app.auth.model.dto.request.UserRequest;
+import com.app.auth.model.dto.response.AuthResponse;
 import com.app.auth.model.dto.response.OperationResponse;
 import com.app.auth.service.IAuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   private final IAuthService authService;
 
-  @PostMapping("/register")
-  public ResponseEntity<OperationResponse> register(@RequestBody UserRequest userRequest) {
-    return new ResponseEntity<>(authService.register(userRequest), HttpStatus.CREATED);
+  @PostMapping("/sign-up")
+  public ResponseEntity<OperationResponse> signUp(@RequestBody @Validated(UserRequest.Create.class) UserRequest userRequest) {
+    return new ResponseEntity<>(authService.createUser(userRequest), HttpStatus.CREATED);
+  }
+
+  @PostMapping("/sign-in")
+  public ResponseEntity<AuthResponse> signIn(@RequestBody @Valid AuthRequest authRequest) {
+    return new ResponseEntity<>(authService.authenticateUser(authRequest), HttpStatus.OK);
   }
 }
