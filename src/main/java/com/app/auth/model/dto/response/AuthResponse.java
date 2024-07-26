558c2e1583cd17391699d9abebbf3d08bf975f7d
Oliver Castillo <olcastillot@gmail.com>
2024-07-25 21:53:42 -0500

    feat: add response DTOs for user

--- src/main/java/com/app/auth/model/dto/response/AuthResponse.java
+++ src/main/java/com/app/auth/model/dto/response/AuthResponse.java
@@ -1,10 +1,4 @@
 package com.app.auth.model.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class AuthResponse {
-  private final String jwt;
+public record AuthResponse(String jwt) {
 }
--- src/main/java/com/app/auth/model/dto/response/OperationResponse.java
+++ src/main/java/com/app/auth/model/dto/response/OperationResponse.java
@@ -1,10 +1,4 @@
 package com.app.auth.model.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class OperationResponse {
-  private final String message;
+public record OperationResponse(String message) {
 }
