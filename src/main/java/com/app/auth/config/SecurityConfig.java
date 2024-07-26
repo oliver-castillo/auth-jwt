451c9de29837293be2b2ff3cbce223212c2454b3
Oliver Castillo <olcastillot@gmail.com>
2024-07-26 16:10:56 -0500

    feat: add security config and user details service

--- src/main/java/com/app/auth/config/SecurityConfig.java
+++ src/main/java/com/app/auth/config/SecurityConfig.java
@@ -1,2 +1,54 @@
-package com.app.auth.config;public class SecurityConfig {
+package com.app.auth.config;
+
+import com.app.auth.service.impl.auth.CustomAccessDeniedHandler;
+import com.app.auth.service.impl.auth.CustomAuthEntryPoint;
+import com.app.auth.service.impl.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CustomAuthEntryPoint customAuthEntryPoint;
+  private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/v1/auth/**").permitAll()
+            .requestMatchers("/api/v1/users/**").authenticated()
+            .anyRequest().denyAll());
+    http.sessionManagement(
+            sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.exceptionHandling(exceptionHandling -> {
+      exceptionHandling.authenticationEntryPoint(customAuthEntryPoint);
+      exceptionHandling.accessDeniedHandler(customAccessDeniedHandler);
+    });
+    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
 }
