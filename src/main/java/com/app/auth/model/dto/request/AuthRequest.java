140ba08e220b29532583f109da8912fcc5f523c4
Oliver Castillo <olcastillot@gmail.com>
2024-07-25 21:55:10 -0500

    feat: add request DTOs for sign in and user

--- src/main/java/com/app/auth/model/dto/request/AuthRequest.java
+++ src/main/java/com/app/auth/model/dto/request/AuthRequest.java
@@ -1,11 +1,17 @@
 package com.app.auth.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class AuthRequest {
-  private final String username;
-  private final String password;
+  @NotBlank(message = "Username is required")
+  private String username;
+
+  @NotBlank(message = "Password is required")
+  private String password;
 }
+
+
