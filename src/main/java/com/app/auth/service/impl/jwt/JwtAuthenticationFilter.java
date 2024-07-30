d77f2ca7ecbe88926bd0614c3702f18d45ed359f
Oliver Castillo <olcastillot@gmail.com>
2024-07-29 23:57:31 -0500

    feat: modify service and dependency

--- src/main/java/com/app/auth/service/impl/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/app/auth/service/impl/jwt/JwtAuthenticationFilter.java
@@ -22,36 +22,37 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-  private final JwtService jwtService;
-  private final UserDetailsService userDetailsService;
-  private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-    final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
-      return;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String jwt = authHeader.replace("Bearer ", "");
+            final boolean isValid = jwtService.verifyTokenAndGetClaims(jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (isValid && authentication == null) {
+                final String username = jwtService.getSubject(jwt);
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
+        }
     }
-
-    try {
-      final String jwt = authHeader.replace("Bearer ", "");
-      final String username = jwtService.verifyTokenAndGetClaims(jwt).getSubject();
-      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (username != null && authentication == null) {
-        UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                null,
-                userDetails.getAuthorities()
-        );
-        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-      }
-      filterChain.doFilter(request, response);
-    } catch (Exception e) {
-      handlerExceptionResolver.resolveException(request, response, null, e);
-    }
-  }
 }
--- src/main/java/com/app/auth/service/impl/jwt/JwtService.java
+++ src/main/java/com/app/auth/service/impl/jwt/JwtService.java
@@ -2,13 +2,11 @@ package com.app.auth.service.impl.jwt;
 
 import com.app.auth.util.exception.InternalErrorException;
 import com.app.auth.util.exception.JwtException;
-import org.jose4j.jws.AlgorithmIdentifiers;
-import org.jose4j.jws.JsonWebSignature;
-import org.jose4j.jwt.JwtClaims;
-import org.jose4j.jwt.consumer.InvalidJwtException;
-import org.jose4j.jwt.consumer.JwtConsumer;
-import org.jose4j.jwt.consumer.JwtConsumerBuilder;
-import org.jose4j.lang.JoseException;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import org.springframework.stereotype.Service;
 
 import java.security.KeyFactory;
@@ -18,94 +16,102 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.text.ParseException;
 import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JwtService {
 
-  private final String issuer = System.getenv("JWT_ISSUER");
-  private final String audience = System.getenv("JWT_AUDIENCE");
+    private final String issuer = System.getenv("JWT_ISSUER");
+    private final String audience = System.getenv("JWT_AUDIENCE");
 
-  private RSAPublicKey getPublicKey() {
-    try {
-      String publicKey = System.getenv("PUBLIC_KEY")
-              .replace("-----BEGIN PUBLIC KEY-----", "")
-              .replace("-----END PUBLIC KEY-----", "")
-              .replace("\n", "")
-              .replaceAll("\\s", "");
-      byte[] decodedKey = Base64.getDecoder().decode(publicKey);
-      X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
-      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      return (RSAPublicKey) keyFactory.generatePublic(keySpec);
-    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-      throw new InternalErrorException("Error setting up public key", e);
+    private RSAPublicKey getPublicKey() {
+        try {
+            String publicKey = System.getenv("PUBLIC_KEY")
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replace("\n", "")
+                    .replaceAll("\\s", "");
+            byte[] decodedKey = Base64.getDecoder().decode(publicKey);
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decodedKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalErrorException("Error setting up public key", e);
+        }
     }
-  }
 
-  private RSAPrivateKey getPrivateKey() {
-    try {
-      String privateKey = System.getenv("PRIVATE_KEY")
-              .replace("-----BEGIN PRIVATE KEY-----", "")
-              .replace("-----END PRIVATE KEY-----", "")
-              .replace("\n", "")
-              .replaceAll("\\s", "");
-      byte[] decodedKey = Base64.getDecoder().decode(privateKey);
-      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKey);
-      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
-    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-      throw new InternalErrorException("Error setting up private key", e);
+    private RSAPrivateKey getPrivateKey() {
+        try {
+            String privateKey = System.getenv("PRIVATE_KEY")
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replace("\n", "")
+                    .replaceAll("\\s", "");
+            byte[] decodedKey = Base64.getDecoder().decode(privateKey);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InternalErrorException("Error setting up private key", e);
+        }
     }
-  }
 
-  private String buildToken(String subject, Map<String, Object> extraClaims) {
-    try {
-      //Set the claims
-      JwtClaims claims = new JwtClaims();
-      claims.setIssuer(issuer);
-      claims.setAudience(audience);
-      claims.setExpirationTimeMinutesInTheFuture(10);
-      claims.setGeneratedJwtId();
-      claims.setIssuedAtToNow();
-      claims.setNotBeforeMinutesInThePast(2);
-      claims.setSubject(subject);
-      for (Map.Entry<String, Object> claim : extraClaims.entrySet()) {
-        claims.setClaim(claim.getKey(), claim.getValue());
-      }
-      //Sign the claims
-      JsonWebSignature jws = new JsonWebSignature();
-      jws.setPayload(claims.toJson());
-      jws.setKey(getPrivateKey());
-      jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
-      return jws.getCompactSerialization();
-    } catch (JoseException e) {
-      throw new JwtException("Error creating token", e);
+    private String buildToken(String subject, Map<String, Object> extraClaims) {
+        try {
+            JWSSigner signer = new RSASSASigner(getPrivateKey());
+            JWTClaimsSet.Builder claimsSetBuilder = new JWTClaimsSet.Builder();
+            JWTClaimsSet claimsSet = claimsSetBuilder
+                    .subject(subject)
+                    .issuer(issuer)
+                    .audience(audience)
+                    .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                    .build();
+            for (Map.Entry<String, Object> entry : extraClaims.entrySet()) {
+                claimsSetBuilder.claim(entry.getKey(), entry.getValue());
+            }
+            SignedJWT signedJWT = new SignedJWT(
+                    new JWSHeader.Builder(JWSAlgorithm.RS256).build(), claimsSet);
+            signedJWT.sign(signer);
+            return signedJWT.serialize();
+        } catch (JOSEException e) {
+            throw new JwtException("Error creating token", e);
+        }
     }
-  }
 
-  public String generateToken(String subject) {
-    return buildToken(subject, new HashMap<>());
-  }
-
-  public String generateToken(String subject, Map<String, Object> claims) {
-    return buildToken(subject, claims);
-  }
-
-  protected JwtClaims verifyTokenAndGetClaims(String jwt) {
-    try {
-      JwtConsumer jwtConsumer = new JwtConsumerBuilder()
-              .setRequireExpirationTime()
-              .setAllowedClockSkewInSeconds(30)
-              .setRequireSubject()
-              .setExpectedIssuer(issuer)
-              .setExpectedAudience(audience)
-              .setVerificationKey(getPublicKey())
-              .build();
-      return jwtConsumer.processToClaims(jwt);
-    } catch (InvalidJwtException e) {
-      throw new JwtException("Error verifying token", e);
+    public String generateToken(String subject) {
+        return buildToken(subject, new HashMap<>());
     }
-  }
+
+    public String generateToken(String subject, Map<String, Object> claims) {
+        return buildToken(subject, claims);
+    }
+
+    protected boolean verifyTokenAndGetClaims(String jwt) {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(jwt);
+            JWSVerifier verifier = new RSASSAVerifier(getPublicKey());
+            boolean isValid = signedJWT.verify(verifier);
+            boolean isExpired = signedJWT.getJWTClaimsSet().getExpirationTime().before(new Date());
+            boolean isIssuerValid = signedJWT.getJWTClaimsSet().getIssuer().equals(issuer);
+            boolean isAudienceValid = signedJWT.getJWTClaimsSet().getAudience().contains(audience);
+            return isValid && !isExpired && isIssuerValid && isAudienceValid;
+        } catch (ParseException | JOSEException e) {
+            throw new JwtException("Error verifying token", e);
+        }
+    }
+
+    protected String getSubject(String jwt) {
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(jwt);
+            return signedJWT.getJWTClaimsSet().getSubject();
+        } catch (ParseException e) {
+            throw new JwtException("Error verifying token", e);
+        }
+    }
+
+
 }
