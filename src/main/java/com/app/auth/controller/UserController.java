4b2cac5e78d25d0c0e0041ea39c6eb5a5c99b2b9
Oliver Castillo <olcastillot@gmail.com>
2024-09-02 14:37:28 -0500

    fix: update files

--- src/main/java/com/app/auth/controller/UserController.java
+++ src/main/java/com/app/auth/controller/UserController.java
@@ -1,19 +1,14 @@
 package com.app.auth.controller;
 
-import com.app.auth.model.dto.request.UserRequest;
+import com.app.auth.model.dto.request.UpdateUserRequest;
 import com.app.auth.model.dto.response.OperationResponse;
 import com.app.auth.model.dto.response.UserResponse;
 import com.app.auth.service.IUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,25 +16,25 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
-  private final IUserService userService;
+    private final IUserService userService;
 
-  @GetMapping("/get-all")
-  public ResponseEntity<List<UserResponse>> getAllUsers() {
-    return ResponseEntity.ok(userService.getAllUsers());
-  }
+    @GetMapping("/get-all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
 
-  @GetMapping("/get/{id}")
-  public ResponseEntity<UserResponse> getUserById(@PathVariable("id") Long id) {
-    return ResponseEntity.ok(userService.getUserById(id));
-  }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
 
-  @PatchMapping("/update")
-  public ResponseEntity<OperationResponse> updateUser(@RequestBody @Validated(UserRequest.Update.class) UserRequest userRequest) {
-    return ResponseEntity.ok(userService.updateUser(userRequest));
-  }
+    @PatchMapping("/update")
+    public ResponseEntity<OperationResponse> updateUser(@RequestBody @Valid UpdateUserRequest request) {
+        return new ResponseEntity<>(userService.updateUser(request), HttpStatus.OK);
+    }
 
-  @DeleteMapping("/delete/{id}")
-  public ResponseEntity<OperationResponse> deleteUser(@PathVariable("id") Long id) {
-    return ResponseEntity.ok(userService.deleteUser(id));
-  }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<OperationResponse> deleteUser(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(userService.deleteUser(id), HttpStatus.OK);
+    }
 }
